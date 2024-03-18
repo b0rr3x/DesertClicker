8e80bb879ae8b8a4fb3f079bbe5ff9b107fd8b53
Samuel Borrego <92246268+b0rr3x@users.noreply.github.com>
2024-03-17 21:21:14 -0600

    Practice Desert Clicker

---
+++ app/src/main/java/com/example/dessertclicker/data/Datasource.kt
@@ -0,0 +1,23 @@
+
+package com.example.dessertclicker.data
+
+import com.example.dessertclicker.R
+import com.example.dessertclicker.model.Dessert
+
+object Datasource {
+    val dessertList = listOf(
+        Dessert(R.drawable.cupcake, 5, 0),
+        Dessert(R.drawable.donut, 10, 5),
+        Dessert(R.drawable.eclair, 15, 10),
+        Dessert(R.drawable.froyo, 30, 20),
+        Dessert(R.drawable.gingerbread, 50, 40),
+        Dessert(R.drawable.honeycomb, 100, 50),
+        Dessert(R.drawable.icecreamsandwich, 500, 60),
+        Dessert(R.drawable.jellybean, 1000, 70),
+        Dessert(R.drawable.kitkat, 2000, 80),
+        Dessert(R.drawable.lollipop, 3000, 90),
+        Dessert(R.drawable.marshmallow, 4000, 100),
+        Dessert(R.drawable.nougat, 5000, 16000),
+        Dessert(R.drawable.oreo, 6000, 20000)
+    )
+}
